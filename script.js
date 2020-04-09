97adae2270896a0db7f5481a2a2bc0e4f2f729c3
Shreyansh84 <shreyanshanand84@gmail.com>
2020-04-09 18:38:35 +0530

    Complete Project

---
+++ script.js
@@ -0,0 +1,12 @@
+/* eslint no-console: 0*/
+var CSS = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+
+color1.addEventListener("input",function(){
+    console.log(color1.value);
+})
+
+color2.addEventListener("input",function(){
+    console.log(color2.value);
+})
